345a9925e1ccdc48b69555a3bbeceb540ec7e050
Jdob11 <jbdobson11@gmail.com>
2024-02-15 07:09:47 -0800

    wrote function to generate profile based on answers to survey

--- survey.js
+++ survey.js
@@ -31,8 +31,8 @@ rl.question('What\'s your name? Nicknames are also acceptable :)', (name) => {
               //call the answers to those questions from the answers object, to make a profile statement
               const paragraph = `${answers.name} is a person! He or she or they do ${answers.activity},
               which is a thing that people do, so stop judging. They listen to ${answers.music}, and
-              probably have since before they were cool... They are often found having ${answers.meal}
-              at the normal time, and lots of the time it's ${answers.favoriteFood}! They are also known
+              probably have since before it or they was or were cool... They are often found having ${answers.meal}
+              at the normal time, usually eating ${answers.favoriteFood}, or something else! They are also known
               to enjoy ${answers.sport}, which may be a sport, or something else, and maybe they
               participate, or maybe they just watch! If they were a superhero, their power would probably
               ${answers.superpower}, but they can't think of a cool superhero name, so for now, they're
